663dea2dd89d5fbe6eeb8bc9bbf850c63f4beca8
amos zohar <azamoszohar@gmail.com>
2019-04-01 21:19:48 +0300

    good enough for today

--- src/App.js
+++ src/App.js
@@ -3,44 +3,50 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import Search from './components/search/search';
 import Suggestion from './components/suggestion/suggestion';
-//import SuggestionsList from './components/suggestion/SuggestionsList';
+
 let suggestionList = [];
+//let contoller = new AbortController();
+//let signal = contoller.signal;
+let currentEvent = null;
 
 function App() {
   const [searchValue, setSearchValue] = useState('');
   const [updated, setUpdated] = useState(Date.now());
   return (
     <div className="App">
-      <Search onChange={searchChanged} value={searchValue} update={updateDate} getSuggestions = {getSuggestions} />
+      <Search onChange={searchChanged} value={searchValue} update={updateDate} getSuggestions={getSuggestions} />
     </div>
-  );//<SuggestionsList suggestionList = {suggestionList} />
+  );
   //internal functions
   function searchChanged(e) {
+    currentEvent = e;
+    //contoller.abort();
     let newValue = e.target.value.toString().trim();
-    // if(newValue!==''){
-    //   setImmediate(fetchSuggestions,newValue);
-    // }
-    setImmediate(fetchSuggestions,newValue);
+    setImmediate(fetchSuggestions, newValue, e);
     setSearchValue(newValue);
   }
-  function fetchSuggestions(thingToSearch){
-    fetch(`http://localhost:3000/api/search/${thingToSearch}`)
+  function fetchSuggestions(thingToSearch, e) {
+    fetch(`http://localhost:3000/api/search/${thingToSearch}`)//, { method: 'GET', signal: signal }
       .then(res => res.json())
-      .then(suggestionsFromServerArray => 
-        generateViewAbleReccomendations(suggestionsFromServerArray))
-        .catch(err=>console.log(err));
+      .then(suggestionsFromServerArray =>
+        generateViewAbleReccomendations(suggestionsFromServerArray, e))
+      .catch(err => console.log(err));
   }
 
-  function generateViewAbleReccomendations(array) {
+  function generateViewAbleReccomendations(array, e) {
     suggestionList = [];
-    for (const cell of array) {
-      typeof cell === "string" ? suggestionList.push(<Suggestion sug={cell} />) :
-        cell.name && typeof cell.name === "string" ? suggestionList.push(<Suggestion sug={cell.name} />)
-          : console.log('cell is incompatible: ', cell);
+    //contoller.abort();
+    if (e === currentEvent) {
+      currentEvent = null;//neccesarry?
+      for (const cell of array) {
+        typeof cell === "string" ? suggestionList.push(<Suggestion sug={cell} />) :
+          cell.name && typeof cell.name === "string" ? suggestionList.push(<Suggestion sug={cell.name} />)
+            : console.log('cell is incompatible: ', cell);
+      }
+      updateDate();
     }
-    updateDate();
   }
-  function getSuggestions(){
+  function getSuggestions() {
     return suggestionList;
   }
 
