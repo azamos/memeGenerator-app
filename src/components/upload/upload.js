78a34d6d036b2f378217beb7a12ce73916b4b5ca
amos zohar <azamoszohar@gmail.com>
2019-04-02 15:26:38 +0300

    more style changes

--- src/components/upload/upload.js
+++ src/components/upload/upload.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './upload.css';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-export default function Upload({description,fileName,descriptionChanged,fileChanged}){
-    return(
-        <div className = "Upload">
-            <TextField type = "file" value = {fileName} onChange = {fileChanged}/>
-            <TextField variant = "filled" label = "description" value = {description} onChange = {descriptionChanged}/>
-            <Button variant = "extendedFab">Upload image</Button>
+export default function Upload({ description, fileName, descriptionChanged, fileChanged }) {
+    return (
+        <div className="Upload">
+            <Button>
+                <input type="file" value={fileName} onChange={fileChanged} className={'UploadBtn'} />
+            </Button>
+            <br></br>
+            <TextField variant="filled" label="description" className="Description"
+                value={description} onChange={descriptionChanged}
+            />
+            <br></br>
+            <Button variant="extendedFab">Upload image</Button>
         </div>
     );
 }
