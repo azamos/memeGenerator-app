cf3434a176587aad4f894d0e70f1df9ba57c2b82
amos zohar <azamoszohar@gmail.com>
2019-04-02 17:59:02 +0300

    properly placed logic

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@ import React from 'react';
 import { useState } from 'react';
 import './App.css';
 import Search from './components/search/search';
-import Suggestion from './components/suggestion/suggestion';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Upload from './components/upload/upload';
-
-let suggestionList = [/*<Suggestion sug="example1" />, <Suggestion sug="example2" />, <Suggestion sug="example3" />, <Suggestion sug="example4" />*/];
-//let contoller = new AbortController();
-//let signal = contoller.signal;
-let currentEvent = null;
 
 function App() {
   const [searchValue, setSearchValue] = useState('');
@@ -46,7 +40,7 @@ function App() {
             <Route path="/topUsers" />
             <Route path="/generateAMeme" />
 
-            <Search onChange={searchChanged} value={searchValue} update={updateDate} getSuggestions={getSuggestions} />
+            <Search onChange={searchChanged} value={searchValue} update={updateDate} />
 
             <Link to="/login or register">
               <Button variant="extendedFab">login or register</Button>
@@ -60,37 +54,10 @@ function App() {
   );
   //internal functions
   function searchChanged(e) {
-    currentEvent = e;
     //contoller.abort();
     let newValue = e.target.value.toString().trim();
-    setImmediate(fetchSuggestions, newValue, e);
     setSearchValue(newValue);
   }
-  function fetchSuggestions(thingToSearch, e) {
-    fetch(`http://localhost:3000/api/search/${thingToSearch}`)//, { method: 'GET', signal: signal }
-      .then(res => res.json())
-      .then(suggestionsFromServerArray =>
-        generateViewAbleReccomendations(suggestionsFromServerArray, e))
-      .catch(err => console.log(err));
-  }
-
-  function generateViewAbleReccomendations(array, e) {
-    suggestionList = [];
-    //contoller.abort();
-    if (e === currentEvent) {
-      currentEvent = null;//neccesarry?
-      for (const cell of array) {
-        typeof cell === "string" ? suggestionList.push(<Suggestion sug={cell} />) :
-          cell.name && typeof cell.name === "string" ? suggestionList.push(<Suggestion sug={cell.name} />)
-            : console.log('cell is incompatible: ', cell);
-      }
-      updateDate();
-    }
-  }
-  function getSuggestions() {
-    return suggestionList;
-  }
-
   function updateDate() {
     setUpdated(Date.now());
   }
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import { useEffect } from 'react';
 import './search.css';
 import TextField from '@material-ui/core/TextField';
 import AutoCompleteList from '../autoCompleteList/autoCompleteList';
-
+import Suggestion from '../suggestion/suggestion';
 
 let suggestionList = [];
-export default function Search({ onChange, value, getSuggestions, update }) {
+let currentEvent;
+export default function Search({ onChange, value, update }) {
+    function onChangeCallBackList(e) {
+        currentEvent = e;
+        onChange(e);//Gotta happen immediatley
+        fetchSuggestions(e);
+    }
+    function fetchSuggestions(e) {
+        fetch(`http://localhost:3000/api/search/${e.target.value}`)//, { method: 'GET', signal: signal }
+            .then(res => res.json())
+            .then(suggestionsFromServerArray =>
+                generateViewAbleReccomendations(suggestionsFromServerArray, e))
+            .catch(err => console.log(err));
+    }
 
-    useEffect(() => {
-        console.log('here');
-        let temp = getSuggestions();
-        suggestionList = temp;
-    });
-
+    function generateViewAbleReccomendations(array, e) {
+        suggestionList = [];
+        //contoller.abort();
+        if (e === currentEvent) {
+            currentEvent = null;//neccesarry?
+            for (const cell of array) {
+                typeof cell === "string" ? suggestionList.push(<Suggestion sug={cell} />) :
+                    cell.name && typeof cell.name === "string" ? suggestionList.push(<Suggestion sug={cell.name} />)
+                        : console.log('cell is incompatible: ', cell);
+            }
+            update();
+        }
+    }
     return (
         <div>
-            <TextField variant = "filled" value={value} onChange={onChange} v label='Search' className="Main" />
+            <TextField variant="filled" value={value} onChange={onChangeCallBackList} v label='Search' className="Main" />
             <AutoCompleteList list={suggestionList} />
         </div>
     );
